0f9150d62dadfe7f361ec44fd1ae78449c7ebe2f
sathishdsgithub <sathish.damodaran.suresh@gmail.com>
2018-11-20 16:20:12 +0800

    Python code to check the VT hash lookup

---
+++ VT_check_hash.py
@@ -0,0 +1,24 @@
+import urllib
+import urllib2
+import json
+import sys
+
+hash_value = sys.argv[1]
+vt_url = "https://www.virustotal.com/vtapi/v2/file/report"
+api_key = "PUT YOUR API KEY HERE"
+parameters = {'apikey': api_key, 'resource': hash_value}
+encoded_parameters = urllib.urlencode(parameters)
+request = urllib2.Request(vt_url, encoded_parameters)
+response = urllib2.urlopen(request)
+json_response = json.loads(response.read())
+if json_response['response_code']:
+    detections = json_response['positives']
+    total = json_response['total']
+    scan_results = json_response['scans']
+    print "Detection : %s%s" % (detections, total)
+    print "VirusTotal Results"
+    for av_name, av_data in scan_results.items():
+        print "\t%s ==> %s" %(av_name, av_data['result'])
+
+else:
+    print "No AV detection"
