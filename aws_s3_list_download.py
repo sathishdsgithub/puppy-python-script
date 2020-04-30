92561ffd7f0299a4f654797f44c3637c7a86b162
sathishdsgithub <sathish.damodaran.suresh@gmail.com>
2020-04-30 09:54:08 +0200

    Update aws_s3_list_download.py

--- aws_s3_list_download.py
+++ aws_s3_list_download.py
@@ -15,23 +15,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-list',action='store_true',help="List the files in aws S3")
 parser.add_argument('-download',action='store_true',help="Download the files in AWS S3")
 
-ACCESS_KEY='XXXXXXXXXXXXXXXXXXX'
-SECRET_KEY='XXXXXXYYYYYZZZZZZZZZZZZZXXXXX'
+ACCESS_KEY='XXXXXXXYYYY'
+SECRET_KEY='XXXUUUHHTTTT'
 args = parser.parse_args()
 if args.list:
     session = Session(aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
     s3 = session.resource('s3')
-    your_bucket = s3.Bucket('XXXYYSSSDDD')
+    your_bucket = s3.Bucket('AA***HHHHHH')
     for s3_file in your_bucket.objects.all():
         print(s3_file.key)
 
 if args.download:
-    file = input('File to download specify with "XXX": ')
+    file = str(input('File to download specify with "XXX": '))
     session = Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
     s3 = session.resource('s3')
-    your_bucket = s3.Bucket('XXXXXYYYYSSSSDDD')
+    your_bucket = s3.Bucket('AAKKOOUUYY^^^')
     for s3_file in your_bucket.objects.all():
-        your_bucket.download_file('str(file)','str(file)')
+        your_bucket.download_file(file,file)
 
 
 
